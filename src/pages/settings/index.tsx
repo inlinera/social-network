c9e9d9f99aee2d7ae2761462a62c45c5edd68797
Inlinera <ilkvakovv@gmail.com>
2024-12-22 01:45:43 +0500

    refactor

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,24 +1,31 @@
 import { useState } from 'react'
 import s from './index.module.scss'
-import { items } from '@/shared/data/settings-list'
+import { items, ISetting } from '@/shared/data/settings-list'
 
 export const SettingsPage = () => {
-  const [activeType, setActiveType] = useState(items[0].type)
+  const [activeType, setActiveType] = useState<ISetting>(items[0])
 
   return (
     <div className={`${s.mainSettings} flex fdc aic jcc`}>
       <div className={`${s.mainSettings__buttons}`}>
         {items.map(t => (
           <button
-            className={`${activeType == t.type && s.active} cw`}
-            onClick={() => setActiveType(t.type)}
-            key={t.type}
+            className={`${activeType === t && s.active} cw`}
+            onClick={() => setActiveType(t)}
+            key={t.name}
           >
             {t.name}
           </button>
         ))}
       </div>
-      <div className={`${s.mainSettings__info}`}></div>
+      <div className={`${s.mainSettings__info}`}>
+        {activeType.content?.map((item, id: number) => (
+          <div key={id}>
+            <span>{item.name}: </span>
+            {item.value ? <span>{item.value}</span> : item.content && item.content}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
