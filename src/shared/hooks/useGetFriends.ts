fd9a03d41f340a8855f335374fb13af561ecd1a3
Inlinera <ilkvakovv@gmail.com>
2025-03-07 22:59:35 +0500

    fix: 1) dependencies (mobx-toolbox upgraded to 1.3.5, etc.), 2) useGetFriends hook error

--- src/shared/hooks/useGetFriends.ts
+++ src/shared/hooks/useGetFriends.ts
@@ -3,11 +3,9 @@ import { IFriend } from '../interfaces/IFriend'
 export const useGetFriends = (targetUserInfo: IFriend, myUserInfo: IFriend) => {
   const targetUserInformation = {
     displayName: targetUserInfo?.displayName,
-    avatarUrl: targetUserInfo?.avatarUrl,
   }
   const myUserInformation = {
     displayName: myUserInfo?.displayName,
-    avatarUrl: myUserInfo?.avatarUrl,
   }
   return [targetUserInformation, myUserInformation]
 }
