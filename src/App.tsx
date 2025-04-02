272d62de0e21a188721772fea8620ce74d0dc56c
Inlinera <ilkvakovv@gmail.com>
2025-04-02 18:56:18 +0500

    feat: context menu disabled

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { CountdownTimer } from './components/timer'
 
 export const App = () => {
   return (
-    <div className="app">
+    <div className="app" onContextMenu={e => e.preventDefault()}>
       <img src="https://i.postimg.cc/1zSXZ47X/logo.png" alt="" width={250} height={250} draggable={false} />
       <CountdownTimer />
     </div>
