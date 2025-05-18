f2f22189ba3f4c082ce7ff530ae8a18358ba3d59
Inlinera <ilkvakovv@gmail.com>
2025-05-18 16:08:24 +0500

    fix: isVisible

--- src/shared/constants/isContentVisible.ts
+++ src/shared/constants/isContentVisible.ts
@@ -1,6 +1,5 @@
 import authApi from '../store/api/user/auth/auth-api'
 import userApi from '../store/api/user/profile/user-api'
-import { myUserFriend } from './users/my-user-info'
 
 /**
  * Function that checks if user can view information
@@ -10,10 +9,13 @@ export const isVisible = (visible: boolean): boolean => {
   const { user } = authApi
   const { userInfo } = userApi
 
+  const isUserFriend = Boolean(user?.friends?.some(friend => friend.displayName === userInfo?.displayName))
+  const isOutgoingReq = Boolean(userInfo?.outgoingReq?.some(req => req.displayName === user?.displayName))
+
   return (
     (visible && !userInfo?.isPrivate) ||
     user?.displayName === userInfo?.displayName ||
-    userInfo?.friends.includes(myUserFriend()) ||
-    userInfo?.outgoingReq.includes(myUserFriend())
+    isOutgoingReq ||
+    isUserFriend
   )
 }
